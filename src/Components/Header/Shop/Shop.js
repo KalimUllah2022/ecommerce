df7ab0ed173c36921709a98092798be214666114
KalimUllah2022 <villageprogrammer2022@gmail.com>
2022-03-27 17:07:42 +0600

    commit

---
+++ src/Components/Header/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Shop.css';
+const Shop = () => {
+    return (
+        <div className="shopContainer">
+            <div className="productsContainer">
+                <h3>This is for products</h3>
+            </div>
+            <div className="cardContainer">
+                <h4>Order Summary</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
